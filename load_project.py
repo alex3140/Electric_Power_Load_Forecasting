ffebd3f68abeee5746d16e08c2991d2dfa2de843
Alexey <alex314@umd.edu>
2016-06-23 09:56:47 -0400

    Main script

--- load_project.py
+++ load_project.py
@@ -1,65 +1,68 @@
 #Script 1: Model Development
 #Builds forecasting models based on linear regression, tree bagging,
 #and gradient boosting
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
+import seaborn as sns 
 from sklearn import linear_model
+from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import GradientBoostingRegressor, BaggingRegressor
 
-merged_data=pd.read_csv(
+merged_data = pd.read_csv(
             'C:\\Users\\alex314\\Desktop\\LoadProject\\merged_data.csv')
-
+            
 #Select the data corresponding to the NYC zone
-modeldata=merged_data[['Date','N_Y_C_','TemperatureKLGA_1','TemperatureKLGA_2']]
-modeldata.columns=['Date','Load','Temperature','DewPoint']
+modeldata = merged_data[['Date','N_Y_C_','TemperatureKLGA_1','TemperatureKLGA_2']]
+modeldata.columns = ['Date','Load','Temperature','DewPoint']
 
 #Create temporal predictors
-modeldata['Date']=pd.to_datetime(modeldata.Date, format='%d-%b-%Y %H:%M:%S')
+modeldata['Date'] = pd.to_datetime(modeldata.Date, format='%d-%b-%Y %H:%M:%S')
 
-modeldata['Hour']=pd.Series(
-                    [modeldata.Date[idx].hour for idx in modeldata.index])
-modeldata['Year']=pd.Series(
-                    [modeldata.Date[idx].year for idx in modeldata.index])
-modeldata['Month']=pd.Series(
-                    [modeldata.Date[idx].month for idx in modeldata.index])
+modeldata['Hour'] = pd.Series(
+                    [modeldata.Date[idx].hour for idx in modeldata.index],
+                    index = modeldata.index)
+modeldata['Year'] = pd.Series(
+                    [modeldata.Date[idx].year for idx in modeldata.index],
+                    index = modeldata.index)
+modeldata['Month'] = pd.Series(
+                    [modeldata.Date[idx].month for idx in modeldata.index],
+                    index = modeldata.index)
 
 #There are important differences in load between weekdays and weekends
 #Create suitable predictors:
-modeldata['DayOfWeek']=pd.Series(
-    [modeldata.Date[idx].isoweekday() for idx in modeldata.index])
-modeldata['isWeekend']=pd.Series(
- [int(modeldata.Date[idx].isoweekday() in [1,7]) for idx in modeldata.index])
+modeldata['DayOfWeek'] = pd.Series(
+    [modeldata.Date[idx].isoweekday() for idx in modeldata.index],
+    index = modeldata.index)
+modeldata['isWeekend'] = pd.Series(
+ [int(modeldata.Date[idx].isoweekday() in [1,7]) for idx in modeldata.index],
+ index = modeldata.index)
 
 #Lagged predictors:
-modeldata['PriorDay']=modeldata.Load.shift(24)
-modeldata['PriorWeek']=modeldata.Load.shift(168)
+modeldata['PriorDay'] = modeldata.Load.shift(24)
+modeldata['PriorWeek'] = modeldata.Load.shift(168)
 
-modeldata=modeldata.dropna()
+modeldata = modeldata.dropna()
         
 #Plot electric power load vs. time
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.plot_date(modeldata.Date[:45000], modeldata.Load[:45000],\
-            'b-',tz=None, xdate=True, ydate=False)
+            'b-',tz = None, xdate = True, ydate = False)
 ax.set_title('Electric Power Load for New York City Zone')
 ax.set_ylabel('Electric Load, MW')
 
 features=['Hour', 'Month','DayOfWeek','Temperature', \
             'isWeekend','DewPoint','PriorDay','PriorWeek']
-X=modeldata[features]
+X = modeldata[features]
 Y = modeldata.Load
 
 #Scatter plots
-g=sns.PairGrid(modeldata,vars=['Load', 'Temperature', 'PriorDay'])
+g = sns.PairGrid(modeldata, vars = ['Load', 'Temperature', 'PriorDay'])
 g = g.map_diag(plt.hist, edgecolor="w")
 g = g.map_offdiag(plt.scatter, edgecolor="w", s=20)
 
-font = {'family' : 'normal', 'size'   : 20}
-matplotlib.rc('font', **font)
 fig = plt.figure()
 ax1 = fig.add_subplot(2, 2, 1)
 ax1.scatter(X.Month, Y)
@@ -81,21 +84,20 @@ ax2.set_xlabel("Hour")
 ax2.set_ylabel("Load, MW")
 
 #Split the data into testing and training sets
-Xtrain, Xtest=X[:45000], X[45000:50000]
-Ytrain, Ytest=Y[:45000], Y[45000:50000]
-DateTest=modeldata.Date[45000:50000]
+Xtrain, Xtest = X[:45000], X[45000:50000]
+Ytrain, Ytest = Y[:45000], Y[45000:50000]
+DateTest = modeldata.Date[45000:50000]
 
-##############################################
 #Linear regression model
 lm = linear_model.LinearRegression()
 lm.fit(Xtrain, Ytrain)
 
 print('Coefficients: \n', lm.coef_)
 print("Residual sum of squares: %.2f"%np.mean((lm.predict(Xtest) - Ytest) ** 2))
-print('Variance score: %.2f' % lm.score(Xtest, Ytest))
+print('R-square: %.2f' % r2_score(Ytest, lm.predict(Xtest)))
 
 #MSE on testing and training sets
-MSEs_lm=[mean_squared_error(
+MSEs_lm = [mean_squared_error(
     Ytest, lm.predict(Xtest)), mean_squared_error(Ytrain, lm.predict(Xtrain))]
 
 print 'MSE on the training set:',mean_squared_error(Ytrain, lm.predict(Xtrain))
@@ -117,13 +119,9 @@ ax2.set_ylabel('Error, MW')
 ax2.set_title('Error between actual and predicted load')
 ax1.legend()
 
-###################################
 #Gradient Boosting Model
-#Default parameters:
-#n_estimators=100- number of boosting stages to perform
-#learning_rate=.1 shrinks the contribution of each tree by learning_rate
-#max_depth=3-maximum depth of the individual regression estimators
-gradBoost=GradientBoostingRegressor()
+gradBoost = GradientBoostingRegressor(learning_rate = .1, 
+                            n_estimators = 400, max_depth = 3)
 gradBoost.fit(Xtrain, Ytrain)
 
 #MSE on testing and training sets
@@ -150,13 +148,15 @@ ax2.set_title('Error between actual and predicted loads')
 ax2.set_ylabel("Error, MW")
 
 featImportances=gradBoost.feature_importances_
-pos= arange(len(features))+0.7
+pos = np.arange(len(features))
+pairs = zip(features, featImportances)
+sorted_pairs = sorted(pairs, key = lambda pair: pair[1])
+features_sorted, featImportances_sorted = zip(*sorted_pairs)
 fig, ax = plt.subplots()
-plt.yticks(pos,features)
-plt.barh(pos,featImportances, 1, color="blue")
-ax.set_title('Gradient Boosting: Feature Importance')
+plt.barh(pos, featImportances_sorted, 1, color = "blue")
+plt.yticks(pos,features_sorted)
+ax.set_title('Gradient Boosting: Relative Feature Importance')
 
-##############################################################################
 #Tree Bagging
 TreeBagger=BaggingRegressor()
 TreeBagger.fit(Xtrain, Ytrain)
@@ -174,7 +174,6 @@ ax2.set_title('Error between actual and predicted loads, MW')
 
 MSEs_Bagging=[mean_squared_error(Ytest, TreeBagger.predict(Xtest)), mean_squared_error(Ytrain, TreeBagger.predict(Xtrain))]
 
-########################################
 #Model Comparison: Bar charts
 fig, ax = plt.subplots()
 width=.3
